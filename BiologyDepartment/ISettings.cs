4403eb7bba39f0b22a783e7ef1d4e3b9cab420ff
jld12345 <jamesl33davis@gmail.com>
2016-05-17 11:13:01 -0500

    Adding database strings

---
+++ BiologyDepartment/ISettings.cs
@@ -0,0 +1,9 @@
+﻿namespace BiologyDepartment.Properties
+{
+    public interface ISettings
+    {
+        string MyActiveDirectory { get; set; }
+        string MyDeployR { get; set; }
+        string MyPostgress { get; set; }
+    }
+}
