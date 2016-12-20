87499d40c5e5137c8822a5ff2d9eae3a8283586d
jld12345 <jamesl33davis@gmail.com>
2016-12-19 23:54:01 -0600

    adding rdao

---
+++ BiologyDepartment/R_Scripts/RDao.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+using Npgsql;
+using NpgsqlTypes;
+using System.Data;
+using System.Windows.Forms;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace BiologyDepartment.R_Scripts
+{
+    public class RDao
+    {
+        #region Private Variables
+        private DataSet ds = new DataSet();
+        private NpgsqlCommand NpgsqlCMD;
+        private NpgsqlDataAdapter adapter;
+        private int rowCountValue = -1;
+        #endregion
+
+        #region Constructor
+        public RDao() { }
+        #endregion
+
+        #region public Methods
+
+        public DataSet GetRScripts()
+        {
+            NpgsqlCMD = new NpgsqlCommand();
+            NpgsqlCMD.CommandText = @"select r_scripts_id, r_scripts_title, r_scripts_body, '' info_btn, '' execute_btn
+                                       from r_scripts
+                                       where is_active = 'Y'";
+            ds = new DataSet();
+            ds = GlobalVariables.GlobalConnection.readData(NpgsqlCMD);
+            if (ds != null)
+            {
+                return ds;
+            }
+            else
+                return null;
+        }
+        #endregion
+    }
+}
